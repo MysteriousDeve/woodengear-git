4a2a8b72e8eddd963f240505ad591136ba8ea273
MysteriousDeve <88138142+MysteriousDeve@users.noreply.github.com>
2022-02-02 11:24:06 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
+require('dotenv').config();
 
-// Configure logger settings
-
-logger.remove(logger.transports.Console);
-
-logger.add(new logger.transports.Console, {
-
-    colorize: true
-});
-
-logger.level = 'debug';
-
-// Initialize Discord Bot
-
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+var bot = new Discord.Client();
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -54,3 +38,5 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
     }
 });
+
+bot.login(process.env.BOT_TOKEN);
